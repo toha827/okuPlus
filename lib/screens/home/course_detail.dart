4930cbebfaf73da3fec4f0c73467e0832a4a491f
Aktilek <tohakktl@gmail.com>
2020-04-08 05:08:19 +0600

    Before Facebook Ingregration

--- lib/screens/home/course_detail.dart
+++ lib/screens/home/course_detail.dart
@@ -6,6 +6,9 @@ import 'package:flutterapp/models/course.dart';
 import 'package:flutterapp/models/myCourse.dart';
 import 'package:flutterapp/services/auth.dart';
 import 'package:flutterapp/services/myCourses.dart';
+import 'package:flutterapp/shared/cardDetatil.dart';
+import 'package:flutterapp/shared/common.dart';
+import 'package:flutterapp/shared/textStyle.dart';
 
 class CourseDetail extends StatefulWidget {
   Course course;
@@ -44,44 +47,146 @@ class _CourseDetailState extends State<CourseDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<String>(
+    return new Scaffold(
+              body: new Container(
+            constraints: new BoxConstraints.expand(),
+            color: new Color(0xFF736AB7),
+            child: new Stack (
+              children: <Widget>[
+                _getBackground(),
+                _getGradient(),
+                _getContent(),
+                _getToolbar(context),
+              ],
+            ),
+          ),
+        );
+  }
+  Container _getBackground () {
+    var _opacity = 1.0;
+    var _xOffset = 0.0;
+    var _yOffset = 0.0;
+    var _blurRadius = 10.0;
+        return new Container(
+              child:  Image.network(course.image,
+                fit: BoxFit.cover,
+                height: 300.0,
+              ),
+            constraints: new BoxConstraints.expand(height: 300.0),
+        );
+  }
+
+  Container _getGradient() {
+        return new Container(
+          margin: new EdgeInsets.only(top: 190.0),
+          height: 110.0,
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              colors: <Color>[
+                new Color(0x00736AB7),
+                new Color(0xFF736AB7)
+              ],
+              stops: [0.0, 0.9],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(0.0, 1.0),
+            ),
+          ),
+        );
+      }
+
+  Widget _getContent() {
+        return StreamBuilder<String>(
         stream: _auth.CurrentUser.map((event) => event.uid),
         builder: (context, snapshot) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text("Second Route"),
+          return Container(
+            child: new ListView(
+              padding: new EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
+              children: <Widget>[
+                new PlanetSummary(course,
+                  horizontal: false,
+                ),
+                new Container(
+                  padding: new EdgeInsets.symmetric(horizontal: 32.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text(course.name,
+                        style: Style.headerTextStyle,),
+                      new Separator(),
+                      new Text(
+                          course.description, style: Style.commonTextStyle),
+                    ],
+                  ),
+                ),
+                new Visibility(
+                      visible: !isBought,
+                      child: RaisedButton(
+                          color: Colors.blue[400],
+                          child: Text(
+                            'Add Course',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {
+                            list.add(course);
+                            _myCoursesService.updateUserData(MyCourse(myCourses: list));
+                          }
+                      )
+                ) 
+              ],
             ),
-            body: Column(
-                children: <Widget>[
-            Image(
-            image: NetworkImage(course.image),
-            fit: BoxFit.fill,
-            height: 200,
-          ),
-          SizedBox(height: 10.0),
-          Text(
-          course.name,
-          style: TextStyle(fontSize: 20.0)
-          ),
-          SizedBox(height: 10.0),
-          Text(course.description),
-          new Visibility(
-                  visible: !isBought,
-                  child: RaisedButton(
-                      color: Colors.blue[400],
-                      child: Text(
-                        'Add Course',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        list.add(course);
-                        _myCoursesService.updateUserData(MyCourse(myCourses: list));
-                      }
-                  )
-          )
-        ],
-      ),
-    );
-  });
+          );
+        });
+
   }
+
+  Container _getToolbar(BuildContext context) {
+        return new Container(
+            margin: new EdgeInsets.only(
+                top: MediaQuery
+                    .of(context)
+                        .padding
+                        .top),
+                child: new BackButton(color: Colors.white),
+              );
+      }
+//    return StreamBuilder<String>(
+//        stream: _auth.CurrentUser.map((event) => event.uid),
+//        builder: (context, snapshot) {
+//          return Scaffold(
+//            appBar: AppBar(
+//              title: Text("Second Route"),
+//            ),
+//            body: Column(
+//                children: <Widget>[
+//            Image(
+//            image: NetworkImage(course.image),
+//            fit: BoxFit.fill,
+//            height: 200,
+//          ),
+//          SizedBox(height: 10.0),
+//          Text(
+//          course.name,
+//          style: TextStyle(fontSize: 20.0)
+//          ),
+//          SizedBox(height: 10.0),
+//          Text(course.description),
+//          new Visibility(
+//                  visible: !isBought,
+//                  child: RaisedButton(
+//                      color: Colors.blue[400],
+//                      child: Text(
+//                        'Add Course',
+//                        style: TextStyle(color: Colors.white),
+//                      ),
+//                      onPressed: () {
+//                        list.add(course);
+//                        _myCoursesService.updateUserData(MyCourse(myCourses: list));
+//                      }
+//                  )
+//          )
+//        ],
+//      ),
+//    );
+//  });
+//  }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/models/course.dart';
 import 'package:flutterapp/models/user.dart';
+import 'package:flutterapp/screens/home/course_detail.dart';
 import 'package:flutterapp/screens/wrapper.dart';
 import 'course_list.dart';
 import 'package:flutterapp/screens/home/main_drawer.dart';
@@ -18,27 +19,81 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<Course>>.value(
-      value: DatabaseService().courses,
-      child: Scaffold(
-        backgroundColor: Colors.blue[50],
-        appBar: AppBar(
-          title: Text('OkuPlus'),
-          backgroundColor: Colors.blue[400],
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-                icon: Icon(Icons.person),
-                label: Text('logout'),
-                onPressed: () async {
-                   await _auth.signOut();
-                 //  Navigator.push(context, MaterialPageRoute(builder: (context) => Wrapper()));
-                }
-            )
-          ],
+      return StreamProvider<List<Course>>.value(
+        value: DatabaseService().courses,
+        child: Consumer<List<Course>>(
+          builder: (context, value, _){
+          return Scaffold(
+          backgroundColor: Colors.blue[50],
+          appBar: AppBar(
+            title: Text('OkuPlus'),
+            backgroundColor: Colors.blue[400],
+            elevation: 0.0,
+            actions: <Widget>[
+              FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('logout'),
+                  onPressed: () async {
+                     await _auth.signOut();
+                   //  Navigator.push(context, MaterialPageRoute(builder: (context) => Wrapper()));
+                  }
+              )
+            ],
+          ),
+          drawer: MainDrawer(),
+          body: Scaffold(
+        body: CustomScrollView(
+        slivers: <Widget>[
+          ///First sliver is the App Bar
+          SliverAppBar(
+          ///Properties of app bar
+          backgroundColor: Colors.white,
+          floating: false,
+          pinned: true,
+          expandedHeight: 200.0,
+
+          ///Properties of the App Bar when it is expanded
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            title: Text(
+              "SliverGrid Widget",
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,),
+            ),
+            background: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Colors.black26,
+                    width: 1.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
-        drawer: MainDrawer(),
-        body: CourseList(),
+        SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            ///no.of items in the horizontal axis
+            crossAxisCount: 2,
+          ),
+          ///Lazy building of list
+          delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+              /// To convert this infinite list to a list with "n" no of items,
+              /// uncomment the following line:
+                  if (index >= value.length) return null;
+              return listItem(value[index], context);
+            },
+            /// Set childCount to limit no.of items
+            /// childCount: 100,
+          ),
+        )
+        ],
+      ),
+      ),
         floatingActionButton: StreamBuilder<bool>(
               stream: _auth.isTeacher,
               builder: (context, snapshot) {
@@ -53,8 +108,42 @@ class Home extends StatelessWidget {
                 ),
                 );
               }),
-      )
+      );
+          })
     );
   }
 
+Widget listItem(Course course, context) => Container(
+  height: 300.0,
+  color: Colors.white,
+  child: Center(
+    child: Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Column(
+        children: <Widget>[
+          Image(
+            height: 100,
+            width: 100,
+            image: NetworkImage(course.image),
+            fit: BoxFit.fill,
+          ),
+          ListTile(
+              onTap: () async {
+                  String uid;
+                  await _auth.CurrentUser.listen((event) { uid = event.uid; });
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => CourseDetail(course, uid)));
+              },
+              title: Text(course.name)
+          )
+        ],
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 5,
+      margin: EdgeInsets.all(10),
+    ),
+  ),
+);
 }
